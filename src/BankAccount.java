c59ebb0ef90f9110761235730a0fbbce0b82a81f
cielliyuanpeng <443760257@qq.com>
2018-01-30 12:49:06 +0800

    bankAccount框架

---
+++ src/BankAccount.java
@@ -0,0 +1,37 @@
+
+public abstract class BankAccount {
+	protected int balanceInPennies;
+	protected double interestRate;
+	protected String accountNumber;
+	public BankAccount(int balance, double interest, String acctNum) {
+		this.balanceInPennies = balance;
+		this.interestRate = interest;
+		this.accountNumber = acctNum;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNo) {
+		this.accountNumber = accountNo;
+	}
+	public boolean credit(int amountPennies){
+		return false;
+	}
+	public abstract boolean debit(int amountPennies);
+	public int getBalanceInPennies() {
+		return balanceInPennies;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double rate) {
+		this.interestRate = rate;
+	}
+	public abstract void applyInterest();
+	@Override
+	public String toString() {
+		return "BankAccount [balanceInPennies=" + balanceInPennies + ", interestRate=" + interestRate
+				+ ", accountNumber=" + accountNumber + "]";
+	}
+	
+}
