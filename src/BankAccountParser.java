9370ae8fe8154f09e14e9105e265a45a6805f97e
cielliyuanpeng <443760257@qq.com>
2018-01-31 08:40:58 +0800

    return new object

--- src/BankAccountParser.java
+++ src/BankAccountParser.java
@@ -3,20 +3,21 @@ public class BankAccountParser {
   public static BankAccount parseStringToBankAccount(String lineToParse) {
     String[] dataArray = lineToParse.split("/");
     switch (dataArray[0]) {
-      case "SA": return SavingsAccount(
+      case "SA": return new SavingsAccount(
         Integer.parseInt(dataArray[1]),
         Double.parseDouble(dataArray[2]),
         dataArray[3]);
-      case "CH": return CheckingAccount(
+      case "CH": return new CheckingAccount(
         Integer.parseInt(dataArray[1]),
         Double.parseDouble(dataArray[2]),
         dataArray[3],
         Integer.parseInt(dataArray[4]));
-      case "CR": return CreditcardAccount(
+      case "CR": return new CreditcardAccount(
         Integer.parseInt(dataArray[1]),
         Double.parseDouble(dataArray[2]),
         dataArray[3],
         Integer.parseInt(dataArray[4]));
     }
+	return null;
   }
 }
